ef486240992cb8e680de8199311042080d0ad4af
Filip Blomqvist <filip.blomqvist.07@gmail.com>
2026-05-02 14:36:22 +0200

    added example of how to switch tabs

--- background.js
+++ background.js
@@ -1,9 +1,63 @@
 chrome.runtime.onMessage.addListener(onMessage);
 
+async function testFunc() {
+  const html = document.querySelector("html");
+  const a = document.createElement("a");
+  a.href = "https://youtube.com";
+  a.textContent = "";
+  html.append(a);
+  a.target = "_blank";
+  a.rel = "noopener";
+  a.click();
+}
+
+async function testFuncTwo() {
+  console.log("test2");
+  await sleep(500);
+  const html = document.querySelector("html");
+  html.innerHTML = "";
+  console.log(chrome.runtime.sendMessage);
+  chrome.runtime.sendMessage({
+    action: "tabSwitch",
+    data: { tabIndex: 0 },
+  });
+
+  async function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
+}
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+  console.log("Querying tabs...");
+  chrome.tabs.query({ index: 1 }, async function (tabs) {
+    console.log(tabs);
+    console.log(activeInfo);
+
+    if (tabs.length == 0) return;
+    const tabId = tabs[0].id;
+    if (activeInfo.tabId != tabId) return;
+    // await chrome.tabs.update(tabId, { active: true, selected: true });
+    console.log("executing script...");
+    console.log(tabs[0]);
+
+    await sleep(1000);
+    console.log(tabs[0]);
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      func: testFuncTwo,
+    });
+  });
+});
+
 function onMessage(message, sender, sendResponse) {
   if (message.action === "executeBackgroundCode") {
     const receivedValue = message.data;
     preExecution(receivedValue);
+  } else if (message.action === "tabSwitch") {
+    chrome.tabs.query({ index: message.data.tabIndex }, async function (tabs) {
+      console.log(tabs);
+      await chrome.tabs.update(tabs[0].id, { active: true, selected: true });
+    });
   }
 }
 
@@ -15,11 +69,10 @@ async function preExecution(data) {
       await chrome.storage.local.set({ data: data }, function () {
         console.log("Data stored successfully.");
       });
-      console.log(data);
 
       chrome.scripting.executeScript({
         target: { tabId: tabId },
-        func: execution,
+        func: testFunc,
       });
     },
   );
@@ -49,4 +102,8 @@ async function execution() {
   async function sleep(time) {
     return new Promise((resolve) => setTimeout(resolve, time));
   }
+}
+
+async function sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
